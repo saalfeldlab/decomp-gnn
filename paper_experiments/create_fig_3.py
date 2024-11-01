7d8aeb96ab748a74e1749f3deba420b0df09e3f2
Michael Innerberger <innerbergerm@hhmi.org>
2024-11-01 11:05:16 -0400

    Add quarto metadata to creation script

--- paper_experiments/create_fig_3.py
+++ paper_experiments/create_fig_3.py
@@ -1,9 +1,17 @@
 # %% [markdown]
-# # Create Figure 3
-# This script creates the first column of Figure 3 in the paper. It generates the data, trains the model, and visualizes
-# the results. Since we ship the trained model with the repository, training can be skipped if desired.
+# ---
+# title: Figure 3
+# author: Cédric Allier, Michael Innerberger, Stephan Saalfeld
+# execute:
+#   echo: false
+# ---
+
+# %% [markdown]
+# This script creates the first column of Figure 3 in the paper: we look at an attraction-repulsion system with three
+# particle types.
 
 # %%
+#| output: false
 import torch
 
 from ParticleGraph.config import ParticleGraphConfig
@@ -13,25 +21,50 @@ from ParticleGraph.plotting import get_figures, load_and_display
 from ParticleGraph.utils import set_device
 
 # %%
-config_list = ['arbitrary_3']
-f_list = ['3']
+#| echo: true
+#| output: false
+# First, we load the configuration file and set the device.
+config_file = 'arbitrary_3'
+figure_id = '3'
+config = ParticleGraphConfig.from_yaml(f'./config/{config_file}.yaml')
+device = set_device("auto")
 
 # %%
-for config_file in config_list:
-    config = ParticleGraphConfig.from_yaml(f'./config/{config_file}.yaml')
-    device = set_device(config.training.device)
-    # data_generate(config, device=device, visualize=True, run_vizualized=0, style='color', alpha=1, erase=True, bSave=True, step=10) # config.simulation.n_frames // config.simulation.n_frames)
-    # data_train(config, config_file, True, device)
-    # data_test (config=config, config_file=config_file, visualize=True, style='color', verbose=False, best_model='0_7500', run=0, step=1, save_velocity=True, device=device) #config.simulation.n_frames // 3, test_simulation=False, sample_embedding=False, device=device)    # config.simulation.n_frames // 7
+#| echo: true
+#| eval: false
+# Subsequently, the data is generated, and the model is trained and tested.
+# Since we ship the trained model with the repository, this step can be skipped if desired.
+data_generate(config, device=device, visualize=True, run_vizualized=0, style='color', alpha=1, erase=True, bSave=True, step=10)
+data_train(config, config_file, True, device)
+data_test(config=config, config_file=config_file, visualize=True, style='color', verbose=False, best_model='0_7500', run=0, step=1, save_velocity=True, device=device)
 
 # %%
-for f in f_list:
-    config_list,epoch_list = get_figures(f, device=device)
+#| echo: true
+#| eval: false
+# Here, we generate the figures that are shown in the first column of Figure 3. The model that has been trained in the
+# previous step is used to generate the rollouts.
+config_list, epoch_list = get_figures(figure_id, device=device)
 
 # %%
-load_and_display('graphs_data/graphs_arbitrary_3/Fig/Fig_0_0.tif', title="Initial configuration for data generation")
-load_and_display('graphs_data/graphs_arbitrary_3/Fig/Fig_0_250.tif', title="Final configuration in data generation")
-load_and_display('log/try_arbitrary_3/results/embedding_arbitrary_3_20.tif', title="Learned embedding")
-load_and_display('log/try_arbitrary_3/results/func_all_arbitrary_3_20.tif', title="Learned functions")
-load_and_display('log/try_arbitrary_3/tmp_recons/Fig_arbitrary_3_0.tif', title="Initial configuration for rollout")
-load_and_display('log/try_arbitrary_3/tmp_recons/Fig_arbitrary_3_192.tif', title="Final configuration in rollout")
+#| fig-cap: "Initial configuration for data generation. The orange, blue, and green particles represent the three different particle types."
+load_and_display('graphs_data/graphs_arbitrary_3/Fig/Fig_0_0.tif')
+
+# %%
+#| fig-cap: "Final configuration after data generation"
+load_and_display('graphs_data/graphs_arbitrary_3/Fig/Fig_0_250.tif')
+
+# %%
+#| fig-cap: "Learned embedding of the particle types"
+load_and_display('log/try_arbitrary_3/results/embedding_arbitrary_3_20.tif')
+
+# %%
+#| fig-cap: "Learned interaction functions"
+load_and_display('log/try_arbitrary_3/results/func_all_arbitrary_3_20.tif')
+
+# %%
+#| fig-cap: "Initial random configuration for rollout"
+load_and_display('log/try_arbitrary_3/tmp_recons/Fig_arbitrary_3_0.tif')
+
+# %%
+#| fig-cap: "Final configuration in rollout, which looks qualitatively very similar to the final configuration of the data generation"
+load_and_display('log/try_arbitrary_3/tmp_recons/Fig_arbitrary_3_192.tif')
