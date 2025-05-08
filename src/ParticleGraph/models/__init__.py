f235c60bd145deeeca602bb1eda4927bdcb2942d
allierc <allierc@janelia.hhmi.org>
2025-05-08 16:08:38 +0200

    remove Mesh_RPS_bis

--- src/ParticleGraph/models/__init__.py
+++ src/ParticleGraph/models/__init__.py
@@ -4,11 +4,10 @@ from .Interaction_Particle_Field import Interaction_Particle_Field
 from .Signal_Propagation import Signal_Propagation
 from .Siren_Network import Siren_Network
 from .Mesh_RPS import Mesh_RPS
-from .Mesh_RPS_bis import Mesh_RPS_bis
 from .Mesh_Laplacian import Mesh_Laplacian
 from .Ghost_Particles import Ghost_Particles
 from .graph_trainer import *
 from .utils import get_embedding, get_embedding_time_series, choose_training_model, constant_batch_size, increasing_batch_size, set_trainable_parameters, set_trainable_division_parameters, plot_training
 
-__all__ = [graph_trainer, Interaction_Particle, Interaction_Particle_Field, Siren_Network, Signal_Propagation, Mesh_RPS, Mesh_RPS_bis, Mesh_Laplacian, Ghost_Particles, get_embedding, get_embedding_time_series, choose_training_model, constant_batch_size,
+__all__ = [graph_trainer, Interaction_Particle, Interaction_Particle_Field, Siren_Network, Signal_Propagation, Mesh_RPS, Mesh_Laplacian, Ghost_Particles, get_embedding, get_embedding_time_series, choose_training_model, constant_batch_size,
            increasing_batch_size, set_trainable_parameters, set_trainable_division_parameters, plot_training, MLP]
